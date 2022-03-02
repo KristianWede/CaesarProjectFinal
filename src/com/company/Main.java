1ba439c181d4cddd482934ecfb3c21f375524ae9
KristianWede <kris79s4@stud.kea.dk>
2022-03-02 17:06:34 +0100

    Added lettersToNumbers and it works

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
@@ -9,7 +10,7 @@ public class Main {
     char[] alfabet = {' ', 'A', 'B', 'C', 'D','E','F','G','H','I','J','K','L','M','N',
             'O','P','Q','R','S','T','U','V','W','X','Y','Z','Æ','Å'};
 
-    public int encryptOrDecrypt(int shift){ //Sprøger om hvis den skal enten encrypt eller decrupy.
+    public int encryptOrDecrypt(int shift){ //Sprøger om hvis den skal enten encrypt eller decrypt.
 
         do{
             System.out.println("Skal du Encrypt eller Decrypt?");
@@ -32,30 +33,65 @@ public class Main {
         int newShift = shift * -1;
         return newShift;
     }
+/*
+    public String solveChar(String besked){ //Brug denne her til alle objekter, som løser koden.
+        for (int i = 0; besked.length() > i ; i++)
+            messageToChar(besked, step);
+    return character;
+    }
+ */
 
-    //public String solveChar(){ //Brug denne her til alle objekter, som løser koden.
+    public char messageToChar(String message, int step){ //Bruger en step variabel, så den ved hvilken del i beskeden skal returneres.
+            char bogstav = message.charAt(step);
+            return bogstav;
+    }
 
-
-    //    return character;
-    //}
+    public int bogstaverTilTal(char inputBogstav){
+        //Går igennem alle bogstaver indtil den finder hvilket tal passer til et char i alfabet Arrayet.
+        for (int i = 0;alfabet.length >= i; i++ ){
+            //Tjekker om alfabet Array er det samme som inputtet.
+            if ( alfabet[i] == inputBogstav){
+                //Returnerer tallet.
+                return i;
+            }
+        }
+        return 404;
+    }
 
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         Main obj = new Main();
+        int step = 0;
 
         //Introduktion til Casesar Cipher
         //Sprøger efter besked og shift.
         System.out.println("Velkommen til Caesar Cipher 2022! v0.5!");
         System.out.println("Venligst indtast en besked som skal kodes: ");
-        String besked = sc.nextLine();
+        String besked = sc.nextLine().toUpperCase();
         System.out.println("Mange tak! Venligst indtast en shift.");
         int shift = sc.nextInt();
+        System.out.println();
 
         //Tager beslutning om shift er til decrypt eller encrypt.
         shift = obj.encryptOrDecrypt(shift);
+
+
         System.out.println(shift);
+        //Skal igennem hele beskeden, og printer den ud én gang, hver gang den looper.
+        for (int i = 0;besked.length() > i ; i++) {
+
+            char bogstav = obj.messageToChar(besked, step);
+
+            int bogstavTal = obj.bogstaverTilTal(bogstav);
+
+            System.out.println(bogstavTal);
+
+            //Tilføjer step til sidst så det næste bogstav bliver valgt til næste gang.
+            step++;
+        }
+
 
 
 
